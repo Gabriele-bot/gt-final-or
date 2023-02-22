eea941bbe4de9da384b0c05500e43029f8017707
gbortola <gabriele.bortolato@cern.ch>
2023-02-22 12:04:29 +0100

    Revert "Fixed mistake on SLR_FinOR_unit"
    This reverts commit 2221d8098104b0128fe90f5223299751d8b8cd77.

--- finor-hdl/firmware/hdl/SLR_FinOR_unit.vhd
+++ finor-hdl/firmware/hdl/SLR_FinOR_unit.vhd
@@ -124,8 +124,8 @@ begin
         port map(
             clk       => clk,
             reset     => rst,
-            ipbus_in  => ipb_to_slaves(N_SLV_CSR),
-            ipbus_out => ipb_from_slaves(N_SLV_CSR),
+            ipbus_in  => ipb_to_slaves(N_SLV_LINK_MASK),
+            ipbus_out => ipb_from_slaves(N_SLV_LINK_MASK),
             d         => stat_reg,
             q         => ctrl_reg,
             qmask     => open,
