5447269046c9bd73f04c92528d403e4d8ce92981
gbortola <gabriele.bortolato@cern.ch>
2023-02-24 19:32:26 +0100

    New mulicycle path timing relaxation
    errata corrige

--- finor-hdl/firmware/ucf/payload_timings.tcl
+++ finor-hdl/firmware/ucf/payload_timings.tcl
@@ -3,7 +3,7 @@ set DEBUG 1
 set_multicycle_path 9 -setup -start -from [get_clocks -filter {NAME =~ clk_payload* }] -to [get_clocks -filter {NAME =~ clks_aux_u_2* }]
 set_multicycle_path 8 -hold -from [get_clocks -filter {NAME =~ clk_payload* }] -to [get_clocks -filter {NAME =~ clks_aux_u_2* }]
 
-if {DEBUG} {
+if {$DEBUG} {
 	set mux_dest_SLRn0 [get_pins -of_objects [get_cells payload/*SLRn0_mux*/*input_reg_reg*] -filter {Name =~ */D}]
 	set mux_clk_SLRn0 [get_pins -of_objects [get_cells payload/*SLRn1_mux*/*input_reg_reg*] -filter {Name =~ */C}]
 	set mux_dest_SLRn1 [get_pins -of_objects [get_cells payload/*SLRn1_mux*/*input_reg_reg*] -filter {Name =~ */D}] 
@@ -13,7 +13,7 @@ if {DEBUG} {
     set_multicycle_path 9 -setup -from [get_pins $mux_clk_SLRn0] -to [get_pins $mux_dest_SLRn0]
     set_multicycle_path 8 -hold  -end  -from [get_pins $mux_clk_SLRn0] -to [get_pins $mux_dest_SLRn0 ]
     
-	set_multicycle_path 9 -setup -from [get_pins $mux_clk_SLRn1] -to [get_pins $mux_dest_SLRn1]
+    set_multicycle_path 9 -setup -from [get_pins $mux_clk_SLRn1] -to [get_pins $mux_dest_SLRn1]
     set_multicycle_path 8 -hold  -end  -from [get_pins $mux_clk_SLRn1] -to [get_pins $mux_dest_SLRn1 ]
     
 }
