e0c8574fa4f99f5671b5bc1191c8e7079f752013
gbortola <gabriele.bortolato@cern.ch>
2023-02-24 19:27:15 +0100

    New multicycle path relaxation

--- finor-hdl/firmware/ucf/payload_timings.tcl
+++ finor-hdl/firmware/ucf/payload_timings.tcl
@@ -4,10 +4,16 @@ set_multicycle_path 9 -setup -start -from [get_clocks -filter {NAME =~ clk_paylo
 set_multicycle_path 8 -hold -from [get_clocks -filter {NAME =~ clk_payload* }] -to [get_clocks -filter {NAME =~ clks_aux_u_2* }]
 
 if {DEBUG} {
-	set mux_input_reg [get_nets -of_objects [get_pins -of_objects [get_cells *payload/*mux*/input_reg*] -filter { DIRECTION == OUT }]]
-	set mux_input_reg_pins [get_pins -leaf -filter {IS_ENABLE} -of $mux_input_reg]
-	set mux_input_reg_cells [get_cells -of $mux_input_reg_pins]
-
-	set_multicycle_path 9 -setup     -from [get_clocks -filter {NAME =~ clks_aux_u_2* }]-to $mux_input_reg_cells
-	set_multicycle_path 8 -hold -end -from [get_clocks -filter {NAME =~ clks_aux_u_2* }]-to $mux_input_reg_cells
+	set mux_dest_SLRn0 [get_pins -of_objects [get_cells payload/*SLRn0_mux*/*input_reg_reg*] -filter {Name =~ */D}]
+	set mux_clk_SLRn0 [get_pins -of_objects [get_cells payload/*SLRn1_mux*/*input_reg_reg*] -filter {Name =~ */C}]
+	set mux_dest_SLRn1 [get_pins -of_objects [get_cells payload/*SLRn1_mux*/*input_reg_reg*] -filter {Name =~ */D}] 
+	set mux_clk_SLRn1 [get_pins -of_objects [get_cells payload/*SLRn1_mux*/*input_reg_reg*] -filter {Name =~ */C}] 
+    
+    
+    set_multicycle_path 9 -setup -from [get_pins $mux_clk_SLRn0] -to [get_pins $mux_dest_SLRn0]
+    set_multicycle_path 8 -hold  -end  -from [get_pins $mux_clk_SLRn0] -to [get_pins $mux_dest_SLRn0 ]
+    
+	set_multicycle_path 9 -setup -from [get_pins $mux_clk_SLRn1] -to [get_pins $mux_dest_SLRn1]
+    set_multicycle_path 8 -hold  -end  -from [get_pins $mux_clk_SLRn1] -to [get_pins $mux_dest_SLRn1 ]
+    
 }
