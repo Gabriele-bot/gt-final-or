8bb313d8e16a3c4da54937aeb1892e4039cadc46
gbortola <gabriele.bortolato@cern.ch>
2023-05-08 20:13:26 +0200

    Padd std_logic_vector in algobits_out module

--- finor-hdl/firmware/hdl/algobits_out.vhd
+++ finor-hdl/firmware/hdl/algobits_out.vhd
@@ -39,7 +39,25 @@ architecture RTL of algobits_out is
 
     signal ctrs_internal : ttc_stuff_t;
 
+    signal algos_in_padded           : std_logic_vector(LWORD_WIDTH * 9 - 1 downto 0);
+    signal algos_after_bxmask_padded : std_logic_vector(LWORD_WIDTH * 9 - 1 downto 0);
+    signal algos_after_prscl_padded  : std_logic_vector(LWORD_WIDTH * 9 - 1 downto 0);
+
 begin
+
+    -- TODO put condition that N algos shuld not be greater than 576 here
+    padd_g : if N_SLR_ALGOS >= LWORD_WIDTH * 9 generate
+        algos_in_padded           <= algos_in(LWORD_WIDTH * 9 - 1 downto 0);
+        algos_after_bxmask_padded <= algos_after_bxmask(LWORD_WIDTH * 9 - 1 downto 0);
+        algos_after_prscl_padded  <= algos_after_prscl(LWORD_WIDTH * 9 - 1 downto 0);
+    else generate
+        algos_in_padded(N_SLR_ALGOS - 1 downto 0)                         <= algos_in;
+        algos_in_padded(LWORD_WIDTH * 9 - 1 downto N_SLR_ALGOS)           <= (others => '0');
+        algos_after_bxmask_padded(N_SLR_ALGOS - 1 downto 0)               <= algos_after_bxmask;
+        algos_after_bxmask_padded(LWORD_WIDTH * 9 - 1 downto N_SLR_ALGOS) <= (others => '0');
+        algos_after_prscl_padded(N_SLR_ALGOS - 1 downto 0)                <= algos_after_prscl;
+        algos_after_prscl_padded(LWORD_WIDTH * 9 - 1 downto N_SLR_ALGOS)  <= (others => '0');
+    end generate;
 
     sync_40_p : process(clk40)
     begin
@@ -50,41 +68,38 @@ begin
 
     mux_algos_before_prscl_out : entity work.mux
         port map(
-            clk360                                              => clk360,
-            rst360                                              => rst360,
-            clk40                                               => clk40,
-            rst40                                               => rst40,
-            bctr                                                => ctrs_internal.bctr,
-            input_40MHz(N_SLR_ALGOS - 1 downto 0)               => algos_in,
-            input_40MHz(LWORD_WIDTH * 9 - 1 downto N_SLR_ALGOS) => (others => '0'),
-            valid_in                                            => algos_valid,
-            output_data                                         => q_algos
+            clk360      => clk360,
+            rst360      => rst360,
+            clk40       => clk40,
+            rst40       => rst40,
+            bctr        => ctrs_internal.bctr,
+            input_40MHz => algos_in_padded,
+            valid_in    => algos_valid,
+            output_data => q_algos
         );
 
     mux_algos_after_prscl_out : entity work.mux
         port map(
-            clk360                                              => clk360,
-            rst360                                              => rst360,
-            clk40                                               => clk40,
-            rst40                                               => rst40,
-            bctr                                                => ctrs_internal.bctr,
-            input_40MHz(N_SLR_ALGOS - 1 downto 0)               => algos_after_bxmask,
-            input_40MHz(LWORD_WIDTH * 9 - 1 downto N_SLR_ALGOS) => (others => '0'),
-            valid_in                                            => algos_valid,
-            output_data                                         => q_algos_after_bxmask
+            clk360      => clk360,
+            rst360      => rst360,
+            clk40       => clk40,
+            rst40       => rst40,
+            bctr        => ctrs_internal.bctr,
+            input_40MHz => algos_after_bxmask_padded,
+            valid_in    => algos_valid,
+            output_data => q_algos_after_bxmask
         );
 
     mux_algos_after_prscl_prvw_out : entity work.mux
         port map(
-            clk360                                              => clk360,
-            rst360                                              => rst360,
-            clk40                                               => clk40,
-            rst40                                               => rst40,
-            bctr                                                => ctrs_internal.bctr,
-            input_40MHz(N_SLR_ALGOS - 1 downto 0)               => algos_after_prscl,
-            input_40MHz(LWORD_WIDTH * 9 - 1 downto N_SLR_ALGOS) => (others => '0'),
-            valid_in                                            => algos_valid,
-            output_data                                         => q_algos_after_prscl
+            clk360      => clk360,
+            rst360      => rst360,
+            clk40       => clk40,
+            rst40       => rst40,
+            bctr        => ctrs_internal.bctr,
+            input_40MHz => algos_after_prscl_padded,
+            valid_in    => algos_valid,
+            output_data => q_algos_after_prscl
         );
 
 end architecture RTL;
