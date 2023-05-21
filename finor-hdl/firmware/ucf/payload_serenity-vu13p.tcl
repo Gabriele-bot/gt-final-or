17014e93e070d9f6f5a5b0e34d904ad907e92829
gbortola <gabriele.bortolato@cern.ch>
2023-05-21 15:23:16 +0200

    Revert to soft floorplanning contraints

--- finor-hdl/firmware/ucf/payload_serenity-vu13p.tcl
+++ finor-hdl/firmware/ucf/payload_serenity-vu13p.tcl
@@ -4,19 +4,20 @@ set SLR_n1  SLR2
 set SLR_n0  SLR0
 set SLR_out SLR1
 
-create_pblock pblock_SLR_n2
-resize_pblock pblock_SLR_n2 -add $SLR_n2
-create_pblock pblock_SLR_n1
-resize_pblock pblock_SLR_n1 -add $SLR_n1
-create_pblock pblock_SLR_n0
-resize_pblock pblock_SLR_n0 -add $SLR_n0
-create_pblock pblock_SLR_out
-resize_pblock pblock_SLR_out -add $SLR_out
+add_cells_to_pblock [get_pblock payload] payload
 
+#add lower row to pblock
+#TODO Modify this
+resize_pblock [get_pblocks payload] -add {CLOCKREGION_X0Y0}
+resize_pblock [get_pblocks payload] -add {CLOCKREGION_X1Y0}
+resize_pblock [get_pblocks payload] -add {CLOCKREGION_X2Y0}
+resize_pblock [get_pblocks payload] -add {CLOCKREGION_X3Y0}
+resize_pblock [get_pblocks payload] -add {CLOCKREGION_X4Y0}
+resize_pblock [get_pblocks payload] -add {CLOCKREGION_X5Y0}
+resize_pblock [get_pblocks payload] -add {CLOCKREGION_X6Y0}
 
-#add_cells_to_pblock [get_pblock payload] payload
+set_property USER_SLR_ASSIGNMENT $SLR_n0  [get_cells -hierarchical -filter {NAME =~ *payload/SLRn0*}]
+set_property USER_SLR_ASSIGNMENT $SLR_out [get_cells -hierarchical -filter {NAME =~ *payload/SLRout*}]
+set_property USER_SLR_ASSIGNMENT $SLR_n1  [get_cells -hierarchical -filter {NAME =~ *payload/SLRn1*}]
+set_property USER_SLR_ASSIGNMENT $SLR_n2  [get_cells -hierarchical -filter {NAME =~ *payload/SLRn2*}]
 
-add_cells_to_pblock [get_pblock pblock_SLR_n2]  [get_cells -hierarchical -filter {NAME =~ *payload/SLRn2*}]
-add_cells_to_pblock [get_pblock pblock_SLR_n1]  [get_cells -hierarchical -filter {NAME =~ *payload/SLRn1*}]
-add_cells_to_pblock [get_pblock pblock_SLR_n0]  [get_cells -hierarchical -filter {NAME =~ *payload/SLRn0*}]
-add_cells_to_pblock [get_pblock pblock_SLR_out] [get_cells -hierarchical -filter {NAME =~ *payload/SLRout*}]
