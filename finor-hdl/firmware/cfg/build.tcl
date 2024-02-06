5ec3cdb78ccbac2cbf84d3e3e5e8a46fd7d99020
gbortola <gabriele.bortolato@cern.ch>
2024-02-07 00:21:29 +0100

    Renable ExtraNetDelay High strategy

--- finor-hdl/firmware/cfg/build.tcl
+++ finor-hdl/firmware/cfg/build.tcl
@@ -10,7 +10,7 @@ set_property STEPS.SYNTH_DESIGN.ARGS.ASSERT true [get_runs synth_1]
 set_property STEPS.SYNTH_DESIGN.ARGS.DIRECTIVE AlternateRoutability [get_runs synth_1]
 
 #set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
-#set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraNetDelay_high [get_runs impl_1]
+set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraNetDelay_high [get_runs impl_1]
 set_property STEPS.PLACE_DESIGN.TCL.POST $lPostPlaceScriptPath [get_runs impl_1]
 
 set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
